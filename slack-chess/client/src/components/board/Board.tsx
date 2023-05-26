fbc47ed1acbe3b1ae26f8564aaa3b08cce4baa18
lavren1974 <lavren1974@gmail.com>
2023-05-26 14:15:07 +0700

    t02

---
+++ slack-chess/client/src/components/board/Board.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './board-styles.css';
+import Cell from '../cell/Cell';
+
+interface Cell {
+    pos: string;
+}
+
+interface BoardProps {
+    cells: Cell[];
+}
+
+const Board: React.FC<BoardProps> = ({ cells }) => {
+    return (
+        <div className="board">
+            {cells.map((cell, index) => (
+                <Cell cell={cell} index={index} key={cell.pos} />
+            ))}
+        </div>
+    );
+};
+
+export default Board;
